c42097eff05f6fe14aed174c056d1dcaaa9b6d2e
SobithaRani-G <143074647+SobithaRani-G@users.noreply.github.com>
2023-10-19 16:19:42 +0530

    Add files via upload

--- DurationDisplay/welcome1.php
+++ DurationDisplay/welcome1.php
@@ -13,23 +13,22 @@ width: 100%; height: 40px;
 <?php
 session_start();
 $stime=$_SESSION['stime'];
-echo "<p align='right'><b>".$_SESSION['stime']."</b></p>"; echo "Welcome : ".$_SESSION['name'];
-?>
-<form name="form1" method="post">
-<input type="submit" name="logout" value="LOGOUT">
-</form>
+echo "<p align='right'><b>".$_SESSION['stime']."</b></p>"; 
+echo "<h2>Goodbye: ".$_SESSION['name'];
+echo "<br/><h2>Login time: ".$_SESSION['stime'];
+echo "<br/><h2>logout time: ".$_SESSION['etime'];
+?>?>
 <?php
 if(isset($_POST['logout']))
 {
+
 $uname=$_POST["uname"]; date_default_timezone_set('Asia/Kolkata');
-
-
 $currentTime = date( 'h:i:s A', time () ); session_start();
 $_SESSION["stime"] = $_SESSION['stime'];
-$_SESSION["etime"] = $currentTime; header('Location: Welcome1.php');
+$_SESSION["etime"] = $currentTime; header('Location: Welcome.php');
 }
 ?>
 <div id = "footer">
-<p align = "center"> &copy; N.USHA SREE all rights --reserved </p>
+<p align = "center"> &copy; N.USHASREE-- all rights -reserved </p>
 </div>
 </html>
